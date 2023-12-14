989d2f2bd08143e2dfe8e36b6c0a3ff001a42836
Michael Wu <mw10013@gmail.com>
2023-12-14 02:43:49 -0500

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import * as jwt from 'jsonwebtoken';
-import { Buffer } from 'node:buffer';
 
 export default {
 	async fetch(request: Request, env: {}, ctx: ExecutionContext): Promise<Response> {
-		globalThis.Buffer = Buffer;
 		const token = jwt.sign('payload', 'SECRET', { expiresIn: '1h' });
 		return new Response(`token: ${token}`);
 	},
