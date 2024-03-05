ba1bbabcfa10c18366e94b38535b070d6bf52267
Michael Wu <mw10013@gmail.com>
2024-03-05 11:30:07 -0500

    paddle-node-sdk-dynamic-import

---
+++ src/README.md
@@ -0,0 +1,12 @@
+- https://worker-sandbox.mw10013.workers.dev/
+- pnpm add @paddle/paddle-node-sdk@github:mw10013/paddle-node-sdk#crypto
+- pnpm add @paddle/paddle-node-sdk-node-prefix@github:mw10013/paddle-node-sdk#e170637f34b76bec39c6de2b4377a3cb1a776eee
+- pnpm add @paddle/paddle-node-sdk-dynamic-import@github:mw10013/paddle-node-sdk#dynamic-import
+
+- https://www.mariokandut.com/how-to-dynamically-load-an-esm-module-in-cjs/
+- https://redfin.engineering/node-modules-at-war-why-commonjs-and-es-modules-cant-get-along-9617135eeca1
+- https://github.com/nodejs/modules/issues/454
+- https://www.reddit.com/r/node/comments/18b09pp/how_to_import_esm_from_cjs_and_should_i/
+- https://adamcoster.com/blog/commonjs-and-esm-importexport-compatibility-examples
+
+- pnpm wrangler deploy --dry-run --outdir dist
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import { Buffer } from 'node:buffer';
 import { generateTOTP } from '@epic-web/totp';
+// import { Environment, Paddle } from '@paddle/paddle-node-sdk';
+// import { Environment, Paddle } from '@paddle/paddle-node-sdk-node-prefix';
+import { Environment, Paddle } from '@paddle/paddle-node-sdk-dynamic-import';
 
-export interface Env {}
+export interface Env {
+	PADDLE_API_KEY: string;
+}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		globalThis.Buffer = Buffer;
 		const totp = generateTOTP();
+		console.log('PADDLE_API_KEY:', env.PADDLE_API_KEY);
+		const paddle = new Paddle(env.PADDLE_API_KEY, {
+			environment: Environment.sandbox,
+		});
+
+		const productCollection = paddle.products.list();
+		const firstPage = await productCollection.next();
+		console.log('First page data', firstPage);
 
 		return new Response(
 			JSON.stringify({
 				totp,
+				firstPage,
 			})
 		);
 	},
